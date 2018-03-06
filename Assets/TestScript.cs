0fe01628ac74a68526f72c9d37a8bd5e8d7a8c28
Chris <chris.g.cowan@gmail.com>
2018-03-06 14:20:56 +0000

    Testing Git Remote

--- Assets/TestScript.cs
+++ Assets/TestScript.cs
@@ -7,6 +7,8 @@ public class TestScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		print ("testing");
+		print ("tesing 2");
+		print ("tesing 3");
 	}
 	
 	// Update is called once per frame
